1541c27688828bfe1eb49c2f97e032113f40ec5c
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-09-29 17:33:02 +0900

    feature/37-vite.config-update

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,9 +7,29 @@ export default defineConfig({
   root: path.resolve(__dirname),
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
   },
   server: {
     host: '0.0.0.0',
     port: 5173
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });

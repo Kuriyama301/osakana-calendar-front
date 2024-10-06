0c326e2a22e82602270ed089fc39c116ed289395
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-10-06 14:48:40 +0900

    fix/44-Calendar-design

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -3,13 +3,8 @@ import MainCalendar from '../components/MainCalendar';
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="bg-blue-600 text-white p-4">
-        <h1 className="text-2xl font-bold">オサカナカレンダー</h1>
-      </header>
-      <main className="flex-grow">
-        <MainCalendar />
-      </main>
+    <div className="flex-grow">
+      <MainCalendar />
     </div>
   );
 };
